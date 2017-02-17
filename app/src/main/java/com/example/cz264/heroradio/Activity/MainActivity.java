f89fc4cd515464f2b9210f44f49401abcfbf6e0b
Allen Zhao <cz264@njit.edu>
2017-02-17 14:36:57 -0500

    add main fragment

---
+++ app/src/main/java/com/example/cz264/heroradio/Activity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.cz264.heroradio.Activity;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cz264.heroradio.Fragments.MainFragment;
+import com.example.cz264.heroradio.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        FragmentManager fm = getSupportFragmentManager();
+//        MainFragment mainFragment = (MainFragment) fm.findFragmentById(R.id.container_main);
+//
+//        if (mainFragment == null) {
+//            mainFragment = MainFragment.newInstance("test1", "test2");
+//            fm.beginTransaction().add(R.id.container_main, mainFragment).commit();
+//        }
+
+        FragmentManager fm = getSupportFragmentManager();
+        MainFragment mainFragment = (MainFragment) fm.findFragmentById(R.id.container_main);
+
+        if (mainFragment == null) {
+            mainFragment = MainFragment.newInstance("test1", "test2");
+            fm.beginTransaction().add(R.id.container_main, mainFragment).commit();
+        }
+    }
+}
